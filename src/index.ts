3879d2cea152b9110890fdee69c2b1bdd3669cf5
soyocar <royomek@gmail.com>
2022-06-05 13:43:39 -0300

    Promedio de edades Promedio de edades en equipo de futbol

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 import "./styles.css";
+function aleatorio(minimo: number, maximo: number): number {
+  return Math.floor(Math.random() * (maximo - minimo - 1)) + minimo;
+}
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+function cargarArreglo(numeroArreglo: number[], dimensionArreglo: number) {
+  let indice: number;
+  for (indice = 0; indice < dimensionArreglo; indice++) {
+    numeroArreglo[indice] = aleatorio(3, 7);
+  }
+}
+
+function mostrarArreglo(numeroArreglo: number[], dimensionArreglo: number) {
+  let indice: number;
+  let linea: string = "";
+  for (indice = 0; indice < dimensionArreglo; indice++) {
+    linea += ` ${numeroArreglo[indice]}`;
+  }
+  console.log(linea);
+}
+
+function obtenerPromedio(numArreglo: number[], dimArreglo: number): number {
+  let prome: number = 0;
+  let sumaTotal: number = 0;
+  let indice: number;
+  for (indice = 0; indice < dimArreglo; indice++) {
+    sumaTotal = sumaTotal + numArreglo[indice];
+  }
+  prome = sumaTotal / dimArreglo;
+  return prome;
+}
+
+//Calcula el promedio de las edades de los jugadores de fútbol
+let promedio: number = 0;
+let dimArreglo: number = Number(prompt(`Indique la cantidad de jugadores: `));
+let numArreglo: number[] = new Array(dimArreglo);
+cargarArreglo(numArreglo, dimArreglo);
+mostrarArreglo(numArreglo, dimArreglo);
+promedio = obtenerPromedio(numArreglo, dimArreglo);
+console.log(`El promedio las edades es de: ${promedio}`);
